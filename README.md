84582c8cccc92feb1ff8458c5c489a745103f4dc
huddeldaddel <35447824+huddeldaddel@users.noreply.github.com>
2022-07-18 12:33:12 +0200

    Implemented first working version

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# jekyll-blog-archiver
-Utility script that creates yearly archive pages for large blogs
+# Jekyll-Blog-Archiver
+
+```
+NAME
+    /home/tom/jekyll-blog-archiver/archive-blog.ps1
+
+SYNOPSIS
+    Creates one archive page per year for blog articles
+
+
+SYNTAX
+    /home/tom/jekyll-blog-archiver/archive-blog.ps1 [-workDir] <String> [<CommonParameters>]
+
+
+DESCRIPTION
+    When using Jekyll you'll find that some themes (like jekyll-theme-potato-hacker) don't support paging in blogs.
+    Large blogs thus get huge and load slowly. This script can be used to create archive pages for old blog posts.
+    It creates one page per year in the _dropdown folder.
+
+
+PARAMETERS
+    -workDir <String>
+        The folder containing the Jekyll website
+
+        Required?                    true
+        Position?                    1
+        Default value
+        Accept pipeline input?       true (ByPropertyName)
+        Accept wildcard characters?  false
+
+    <CommonParameters>
+        This cmdlet supports the common parameters: Verbose, Debug,
+        ErrorAction, ErrorVariable, WarningAction, WarningVariable,
+        OutBuffer, PipelineVariable, and OutVariable. For more information, see
+        about_CommonParameters (https://go.microsoft.com/fwlink/?LinkID=113216).
+
+INPUTS
+
+OUTPUTS
+
+NOTES
+
+
+        The script depends on powershell-yaml. You'll have to install that first via:
+        "Install-Module -Name powershell-yaml"
+
+    -------------------------- EXAMPLE 1 --------------------------
+
+    PS > pwsh archive-blog.ps1 ~/rauschzeit.github.io
+```
--- archive-blog.ps1
+++ archive-blog.ps1
@@ -1,4 +1,31 @@
-Import-Module powershell-yaml                     # Has to be installed via "Install-Module powershell-yaml"
+<#
+    .SYNOPSIS
+        Creates one archive page per year for blog articles
+
+    .DESCRIPTION
+        When using Jekyll you'll find that some themes (like jekyll-theme-potato-hacker) don't support paging in blogs. 
+        Large blogs thus get huge and load slowly. This script can be used to create archive pages for old blog posts. 
+        It creates one page per year in the _dropdown folder.
+
+
+    .PARAMETER workDir
+        The folder containing the Jekyll website    
+
+    .EXAMPLE
+        pwsh archive-blog.ps1 ~/rauschzeit.github.io
+
+    .NOTES
+        The script depends on powershell-yaml. You'll have to install that first via:
+        "Install-Module -Name powershell-yaml"
+#>
+
+param(
+    [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+    [string]$workDir
+)
+
+Import-Module powershell-yaml
+Set-StrictMode -Version 3
 
 class MarkdownFile {
     
@@ -6,10 +33,21 @@ class MarkdownFile {
     [System.Object]$FrontMatter
     [string[]]$Markdown
 
+    # Contructor that gets used to read a file
     MarkdownFile($file) {
         $this.File = $file        
         $this.FrontMatter = $this.GetFrontMatter()
         $this.Markdown = $this.GetMarkdown()
+    }
+
+    # Contructor that gets used to create a file
+    MarkdownFile($filePath, $pageHeader) {
+        if(Test-Path -Path $filePath -PathType Leaf) {
+            Remove-Item -Path $filePath
+        }
+        $this.File = New-Item -ItemType File -Path $filePath
+        $this.FrontMatter = $pageHeader
+        $this.Markdown = @()
     }
 
     hidden [System.Object]GetFrontMatter() {
@@ -48,9 +86,85 @@ class MarkdownFile {
         return $content
     }
 
+    [void]Write() {
+        "---" | Out-File -FilePath $this.File.FullName
+        ConvertTo-Yaml $this.FrontMatter | Out-File -FilePath $this.File.FullName -Append
+        "---" | Out-File -FilePath $this.File.FullName -Append
+        $this.Markdown -join "`r`n" | Out-File -FilePath $this.File.FullName -Append
+    }
+
 }
 
-$files = Get-ChildItem -Path C:\Users\TWerner\Projekte\architektur-doc\docs\07-verteilungssicht -Recurse -Filter "*.md"
-foreach ($file in $files) {
-    $mdFile = [MarkdownFile]::new($file)    
+class ArchivePageHeader {
+
+    [string]$layout
+    [string]$title
+    [string]$dropdown
+    [int]$priority
+
+    ArchivePageHeader([string]$title, [int]$priority) {
+        $this.layout = "page"
+        $this.title = $title
+        $this.dropdown = "Blog-Archiv"
+        $this.priority = $priority
+    }
+
 }
+
+class BlogProcessor {
+
+    [string]$WorkDir    
+
+    BlogProcessor($dir) {
+        $this.WorkDir = $dir                
+    }
+
+    [void]Run() {
+        [MarkdownFile]$archivePage = $null
+        [string]$currentYear = Get-Date -Format "yyyy"
+        [string]$yearBeingProcessed = ""
+        [int]$priority = 1
+        [System.IO.FileSystemInfo[]]$files = Get-ChildItem -Path "$($this.WorkDir)/_posts" -Recurse -include ('*.md', '*.html')
+        foreach ($file in $files) {
+            $blogPostYear = $file.Name[0..3] -join ""
+            if($currentYear -ne $blogPostYear) {                
+                if($yearBeingProcessed -ne $blogPostYear) {
+                    if("" -ne $yearBeingProcessed) {
+                        $archivePage.Write()
+                    }
+                 
+                    [ArchivePageHeader]$header = [ArchivePageHeader]::new("Blog-Archiv $blogPostYear", $priority++)
+                    $archivePage = [MarkdownFile]::new("$($this.WorkDir)/_dropdown/blog-$blogPostYear.md", $header)
+                    $archivePage.Markdown = @("# Blog-Archiv $blogPostYear")                    
+                    $yearBeingProcessed = $blogPostYear
+                }
+                
+                Write-Host $file.Name
+
+                $mdFile = [MarkdownFile]::new($file)
+                $postPrefix = @()
+                $postPrefix += "`r`n"
+                $postPrefix += "<a id=`"$($mdFile.FrontMatter.wordpress_id)`"></a>`r`n"
+                try {
+                    $postPrefix += "## $($mdFile.FrontMatter.title)`r`n"
+                } catch {
+                    $postPrefix += "## Ohne-Titel`r`n"
+                }
+
+                $archivePage.Markdown = $archivePage.Markdown + $postPrefix
+                foreach($line in $mdFile.Markdown) {
+                    # You don't need this: special handling for broken links in rauschzeit.de
+                    [string]$fixedLine = $line -replace "http\:\/\/rauschzeit.de\/\?p=", "#"   
+                    $archivePage.Markdown = $archivePage.Markdown + $fixedLine
+                }
+            }
+        }
+        if($null -ne $archivePage) {
+            $archivePage.Write()
+        }
+    }
+
+}
+
+$processor = [BlogProcessor]::new($workDir)
+$processor.Run()
